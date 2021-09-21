362242c667d20b1dbbd7eb996568396fd6d8846e
Guilherme Amaral <gvma@ic.ufal.br>
2021-09-20 21:22:16 -0300

    Crossover

--- ga.py
+++ ga.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import random
 
 # UM INDIVIDUO REPRESENTA OS PESOS USADOS NA REDE NEURAL
 
@@ -16,9 +17,25 @@ def score(clf, population):
     scores.append(clf.predict(individual))
   return scores
 
+def exchange_dna(father, mother, new_population):
+  first_child = father[:1] + mother[1:]
+  second_child = mother[:1] + father[1:]
+  new_population.append(first_child)
+  new_population.append(second_child)
+  return new_population
+
 # TODO fazer o crossover entre os individuos
-def crossover():
-  pass
+def crossover(new_population, population):
+  for i in range(len(population)):
+    father_index = np.random.randint(0, len(population)-1)
+    mother_index = np.random.randint(0, len(population)-1)
+    while father_index == mother_index:
+      father_index = np.random.randint(0, len(population)-1)
+      mother_index = np.random.randint(0, len(population)-1)
+    father = population[father_index]
+    mother = population[mother_index]
+    new_population = exchange_dna(father, mother, new_population)
+  return new_population
 
 # TODO fazer a mutacao em um individuo
 def mutation():
--- main.py
+++ main.py
@@ -11,13 +11,14 @@ clf = Adaline()
 initial_population = get_initial_population(clf.X)
 scores = score(clf, initial_population)
 scores.sort(reverse=True)
-print(scores)
 population = initial_population
+# print(population)
 
 while True:
   if scores[0] == 35:
     break
-  # new_population = crossover([], population)
+  new_population = crossover([], population)
   # mutation(new_population)
+  break
   # scores = score(clf, population)
   # population = selection(population, new_population)
